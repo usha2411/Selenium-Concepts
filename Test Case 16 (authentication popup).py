6afcfea2ef0ece3dffb6ce83af044571393596d3
usha2411 <ushanazare@gmail.com>
2025-06-26 15:48:54 +0530

    Delete Test Case 16 (authentication popup).py

--- Test Case 16 (authentication popup).py
+++
@@ -1,8 +0,0 @@
-#syntax=http://username:password@test.com
-import time
-
-from selenium import webdriver
-driver=webdriver.Chrome()
-driver.get("http://admin:admin@the-internet.herokuapp.com/basic_auth")
-driver.maximize_window()
-time.sleep(5)
